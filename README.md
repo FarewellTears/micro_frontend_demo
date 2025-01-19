5b9efd3aa3d8410b160634fe078a94e6913d5177
FarewellTears <farewelltears97@gmail.com>
2025-01-20 05:57:57 +0800

    SystemJs 自实现

---
+++ README.md
@@ -0,0 +1,18 @@
+# 微前端 demo 编写
+
+## Part 0. 前言
+
+
+## Part 1. SystemJs 实现
+### 1. 项目搭建
+
+```bash
+mkdir systemjs_demo
+cd systemjs_demo
+pnpm init
+pnpm i webpack webpack-cli webpack-dev-server -D
+pnpm i html-webpack-plugin -D
+pnpm i react react-dom
+pnpm i -D babel-loader @babel/core @babel/preset-env
+```
+
---
+++ systemjs_demo/webpack.config.js
@@ -0,0 +1,51 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = (env) => ({
+  // 1. 为了更好的看到打包后的代码，统一设置 mode 为开发模式 development | production
+  mode: "development",
+  //   entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    // 2. 指定生产模式下采用 systemjs 模块规范
+    libraryTarget: env.production ? "system" : "", // system 为模块规范，此外还有 umd amd esModule commonjs 等
+  },
+  module: {
+    // 3. 使用 babel 解析 js 文件
+    rules: [
+      {
+        test: /\.js$/,
+        // 排除不应参与转码的库
+        exclude: [
+          // \\ for Windows, \/ for Mac OS and Linux
+          /node_modules[\\\/]core-js/,
+          /node_modules[\\\/]webpack[\\\/]buildin/,
+          /node_modules/,
+        ],
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              ["@babel/preset-react", { runtime: "automatic" }],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    // 4. 生产环境下不生成 html
+    !env.production &&
+      new HtmlWebpackPlugin({
+        // template: path.resolve(__dirname, "public/index.html"),
+        template: path.resolve(__dirname, "public/index.html"),
+      }),
+  ].filter(Boolean),
+  // 5. 生产环境下不打包 react, react-dom。（这里也可以打包到当前项目下均可， 综合考虑公共模块是否要打包进去、打包后的资源大小）
+  // [] 选择不在打包过程中加载的模块而通过脚本方式引入
+  externals: env.production ? ["react", "react-dom"] : [],
+});
+
+// 将子应用打包成类库，在主应用中加载这个库（systemjs）
